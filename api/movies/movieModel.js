8da9494077c0d3cdcd6f40a320fc65de36c05ed5
Chen Zhiling <20091576@mail.wit.ie>
2021-01-17 00:52:40 +0800

    Add movie model

---
+++ api/movies/movieModel.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+
+const MovieSchema = new Schema({
+  adult: { type: Boolean },
+  id: { type: Number, required: true, unique: true },
+  poster_path: { type: String },
+  overview: { type: String },
+  release_date: { type: String },
+  original_title: { type: String },
+  genre_ids: [{ type: Number }],
+  original_language: { type: String },
+  title: { type: String },
+  backdrop_path: { type: String },
+  popularity: { type: Number },
+  vote_count: { type: Number },
+  video: { type: Boolean },
+  vote_average: { type: Number },
+  production_countries: [{
+    iso_3166_1: { type: String },
+    name: { type: String }
+  }],
+  runtime: { type: Number },
+  spoken_languages: [{
+    iso_639_1: { type: String },
+    name: { type: String }
+  }],
+  status: { type: String },
+  tagline: { type: String },
+  page: {type: Number},
+  reviews: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Reviews'
+  }, {collection: 'movies'}],
+  upcomingPage: {type: Number}
+});
+
+MovieSchema.statics.findByMovieDBId = function (id) {
+  return this.findOne({ id: id });
+};
+
+module.exports = mongoose.model('Movies', MovieSchema);
+
+
