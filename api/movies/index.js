3311b583328154d7702ca3437fc3fad3a66f72d4
Chen Zhiling <20091576@mail.wit.ie>
2021-01-17 00:56:10 +0800

    Movie route - add upcoming and search

--- api/movies/index.js
+++ api/movies/index.js
@@ -1,26 +1,82 @@
-import express from "express";
-import { getMovies, getMovie, getMovieReviews } from "./tmdb-api";
+const express = require('express');
+const {getMovieByPage, getUpcomingMovies, searchMovies} = require('../tmdb-api');
+const movieModel = require('./movieModel');
 
-const router = express.Router();
+const router = express.Router(); // eslint-disable-line
 
-router.get("/", (req, res, next) => {
-  getMovies()
-    .then((movies) => res.status(200).send(movies))
-    .catch((error) => next(error));
+router.get('/', (req, res, next) => {
+	try {
+		movieModel.find().then(movies => res.status(200).send(movies))
+	} catch (err) {
+		next(err)
+	}
 });
 
-router.get("/:id", (req, res, next) => {
-  const id = parseInt(req.params.id);
-  getMovie(id)
-    .then((movie) => res.status(200).send(movie))
-    .catch((error) => next(error));
+router.get('/page/:page', async (req, res, next) => {
+	const page = parseInt(req.params.page)
+	try {
+		const movie = await movieModel.findOne({ "page": page })
+		if (movie) {
+			console.log('Load in the database')
+			movieModel.find({ "page": page }).then(movies => res.status(200).send(movies)).catch(err => next(err))
+		} else {
+			console.log('Load from the TMDB and store')
+
+			const movies = await getMovieByPage(page)
+			movies.forEach(movie => {
+				movie.page = page
+			})
+			await movieModel.collection.insertMany(movies)
+			res.status(200).send(movies)
+		} 
+	}catch (err) {
+		next(err)
+	}
+})
+
+router.get('/:id', (req, res, next) => {
+	const id = parseInt(req.params.id);
+	movieModel.findByMovieDBId(id).populate("reviews").then(movie => res.status(200).send(movie)).catch(err => next(err));
 });
 
-router.get("/:id/reviews", (req, res, next) => {
-  const id = parseInt(req.params.id);
-  getMovieReviews(id)
-    .then((reviews) => res.status(200).send(reviews))
-    .catch((error) => next(error));
-});
+router.get('/upcoming/:page', async (req, res, next) => {
+	const page = parseInt(req.params.page)
+	try {
+		const movie = await movieModel.findOne({ "upcomingPage": page })
+		if (movie) {
+			console.log('Load in the database')
+			movieModel.find({ "upcomingPage": page }).then(movies => res.status(200).send(movies)).catch(err => next(err))
+		} else {
+			console.log('Load from the TMDB and store')
 
-export default router;
+			const movies = await getUpcomingMovies(page)
+			movies.forEach(movie => {
+				movie.upcomingPage = page
+			})
+			movies.forEach(async movie => {
+				const isExist = await movieModel.findOne({ "id": movie.id })
+				if (!isExist) {
+					await movieModel.create(movie)
+				} else {
+					await movieModel.findOneAndUpdate({ "id": movie.id }, movie)
+				}
+			})
+			res.status(200).send(movies)
+		} 
+	}catch (err) {
+		next(err)
+	}
+})
+
+router.get('/search/:query', async (req, res, next) => {
+	const query_string = "" + req.params.query
+	try {
+		const movies = await searchMovies(query_string)
+		res.status(200).send(movies)
+	} catch(err) {
+		next(err)
+	}
+})
+
+module.exports = router;
+
